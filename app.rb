4102cf8a74f88ccf493d3b5e8266b84cbf34f668
Georg G <nilsding@nilsding.org>
2015-02-03 19:02:38 +0100

    increment this number

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ require 'runscript'
 # Hash containing the configuration.
 CONFIG = YAML.load_file File.expand_path('./config.yml', File.dirname(__FILE__))
 # Version of ASS
-ASS_VERSION = '0.1.0'
+ASS_VERSION = '0.1.1'
 
 cwd = File.expand_path('.', CONFIG[:repo_path])
 FileUtils.mkdir_p(cwd)
